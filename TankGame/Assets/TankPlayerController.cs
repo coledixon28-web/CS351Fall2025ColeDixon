5fd981170b602fc384bdb83edeb30e7fbe584dcc
coledixon28-web <coledixon28@gmail.com>
2025-09-23 20:24:21 -0500

    tankgame

---
+++ TankGame/Assets/TankPlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankPlayerController : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float verticalInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.right * Time.deltaTime * speed * verticalInput);
+
+        // Uncomment if you want constant rotation:
+        // transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime * horizontalInput);
+
+        if (verticalInput < 0)
+        {
+            transform.Rotate(Vector3.back, -turnSpeed * Time.deltaTime * horizontalInput);
+        }
+        else
+        {
+            transform.Rotate(Vector3.back, turnSpeed * Time.deltaTime * horizontalInput); 	
+        }
+    }
+}
